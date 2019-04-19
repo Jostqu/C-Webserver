09181a76b1ca90b842837dd4e48c05ce2bdfae8b
Björn Marx <bm502101@fh-muenster.de>
2019-04-19 20:15:00 +0200

    hashmap hinzugefügt

---
+++ hash.c
@@ -0,0 +1,80 @@
+#include "hash.h"
+
+HashList* SHL_create(Hash firstElement)
+{
+	HashList* hl = calloc(1, sizeof(HashList));
+
+	if (hl == NULL)
+		exit(1);
+
+	hl->data = firstElement;
+
+	return hl;
+}
+
+void SHL_append(HashList* first, Hash next)
+{
+	HashList* b = first;
+	for (; b->next != NULL; b = b->next);
+
+	HashList* newElement = calloc(1, sizeof(HashList));
+
+	if (newElement == NULL)
+		exit(2);
+
+	newElement->data = next;
+	newElement->next = NULL; //should be null anyways because of calloc, but safety first
+
+	b->next = newElement;
+}
+
+void SHL_free_element(HashList* item)
+{
+	string_free(item->data.key);
+	string_free(item->data.value);
+	item->next = NULL;
+	free(item);
+}
+
+void SHL_remove_all(HashList* first)
+{
+	HashList* element = first, *buffer;
+
+	while (element->next != NULL)
+	{
+		buffer = element->next;
+		SHL_free_element(element);
+		element = buffer;
+	}
+
+	SHL_free_element(element);
+}
+
+Hash* SHL_find_key(HashList* first, string* key)
+{
+	HashList* b = first;
+
+	while (b->next)
+	{
+		if (str_cmp_str(&b->data.key, key))
+			return &b->data;
+	}
+
+	return NULL;
+}
+
+Hash* SHL_at(HashList* first, unsigned int index)
+{
+	HashList* b = first;
+
+	while (index-- > 0 && b->next != NULL)
+		b = b->next;
+	
+	return &b->data;
+}
+
+Hash SH_create(string key, string value)
+{
+	Hash hash = { key, value };
+	return hash;
+}
---
+++ hash.h
@@ -0,0 +1,87 @@
+///
+/// \author Björn Marx
+/// \date 12/04/2019.
+/// \file hash.h
+///
+
+#ifndef PSE2018TESTS_HASH_H
+#define PSE2018TESTS_HASH_H
+
+#include  "string.h"
+
+/*!
+ * \brief Hash element 
+ */
+typedef struct StringHashStruct {
+	string* key;	///< key of hash element
+	string* value;	///< corresponding value
+} Hash;
+
+/*!
+ * \brief Structure defining a listElement
+ */
+typedef struct StringHashList {
+	Hash data;						///< Hash value of list element
+	struct StringHashList* next;	///< next list element
+} HashList;
+
+/*!
+ * \brief creates new list
+ * Creates new list, but requires 
+ */
+HashList* SHL_create(Hash firstElement);
+
+/*!
+ * \brief appends a new element at the end
+ */
+void SHL_append(HashList* first, Hash next);
+
+/*!
+ * \brief frees an element in the the list including it's data's Strings
+ * bear in mind that this will NOT touch the *next element 
+ * 
+ * \param item: the list element that shall be freed 
+ */
+void SHL_free_element(HashList* item);
+
+/*!
+ * \brief removes element at position
+ * 
+ * \param first pointer to fist list element
+*/
+//will be implemented if needed
+//bool SHL_remove(HashList* first, unsigned int position);
+
+/*!
+ * \brief deletes all elements starting at first
+ */
+void SHL_remove_all(HashList* first);
+
+/*!
+ * \brief returns pointer to the *FIRST* element containing key
+ * Returns pointer to the *FIRST* element containing key, will return NULL if @key is not found
+ * 
+ * \param first pointer to list
+ * \param key desired key to be found
+ * \return pointer to *first* element with key
+ */
+Hash* SHL_find_key(HashList* first, string* key);
+
+/*!
+ * \brief return index'th element of HashList
+ * function returning the index'th element of HashList first, it will return the last element if index is out of bound
+ * 
+ * \param first first element of list
+ * \param index index of desired element 
+ * \return pointer to Hash object, will return last element if index ist out of range
+ */
+Hash* SHL_at(HashList* first, unsigned int index);
+
+/*!
+ * \brief creates Hash with given @key and @value
+ * 
+ * \return copy of Hash with desired key and value
+ */
+Hash SH_create(string key, string value);
+
+#endif //PSE2018TESTS_HASH_H
