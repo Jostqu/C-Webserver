66709176d412612f9d9e29027885f24637319c09
Marcel Weski <mw293344@fh-muenster.de>
2019-05-05 01:30:27 +0200

    Parsing-Schritte in Untermethoden gepackt

--- request.c
+++ request.c
@@ -57,6 +57,8 @@ HttpResponseCodes validate_path(string *path, string** validatedPath)
 	realpath(tmp->buf, absolutePath->buf);
 	absolutePath->len = strlen(absolutePath->buf);
 
+	string_print(absolutePath);
+
 	string_free(tmp);
 
 	HttpResponseCodes responseCode;
@@ -83,6 +85,145 @@ HttpResponseCodes validate_path(string *path, string** validatedPath)
 	return responseCode;
 }
 
+HttpRequestParsingState parsing_method(char c, string* strMethod, HttpRequest** httpRequest, HttpResponseCodes* responseCode)
+{
+	if (c != ' ')
+	{
+		// Wenn kein Leerzeichen, dann Zeichen an den String anhängen
+		string_add_char(strMethod, c);
+
+		// Weiterhin die Methode parsen
+		return PARSING_METHOD;
+	}
+	else
+	{
+		(*httpRequest)->method = get_method_from_string(strMethod);
+		if ((*httpRequest)->method != GET)
+		{
+			*responseCode = NOT_IMPLEMENTED;
+		}
+
+		// Wenn ein Leerzeichen gefunden wurde, den Pfad parsen
+		return PARSING_PATH;
+	}
+}
+
+HttpRequestParsingState parsing_path(char c, string** strPath, HttpRequest** httpRequest, HttpResponseCodes* responseCode)
+{
+	if (c != ' ')
+	{
+		if (!(*strPath))
+		{
+			*strPath = string_new(PATH_CAPACITY);
+		}
+
+		// Wenn kein Leerzeichen, dann Zeichen an den String anhängen
+		string_add_char(*strPath, c);
+
+		if ((*strPath)->len > PATH_CAPACITY)
+		{
+			*responseCode = BAD_REQUEST;
+		}
+
+		// Weiterhin den Pfad parsen
+		return PARSING_PATH;
+	}
+	else
+	{
+		url_decode(*strPath);
+
+		// Wenn ein Leerzeichen gefunden wurde, die Version parsen
+		return PARSING_VERSION;
+	}
+}
+
+HttpRequestParsingState parsing_version(char c, string* strVersion, HttpRequest** httpRequest, HttpResponseCodes* responseCode)
+{
+	if (c != '\n')
+	{
+		// Wenn keine neue Zeile, dann Zeichen an den String anhängen
+		string_add_char(strVersion, c);
+
+		// Weiterhin die Version parsen
+		return PARSING_VERSION;
+	}
+	else
+	{
+		(*httpRequest)->version = validate_version(strVersion);
+		if ((*httpRequest)->version == UNSUPPORTED)
+		{
+			*responseCode = BAD_REQUEST;
+		}
+
+		// Wenn eine neue Zeile beginnt, Key eines Feldes parsen
+		return PARSING_FIELD_KEY;
+	}
+}
+
+HttpRequestParsingState parsing_field_key(char c, string** strKey)
+{
+	if (c == '\n')
+	{
+		// Wenn beim Parsen des Keys eine neue Zeile gefunden wird, sind die Felder zu Ende und es folgen die Daten
+		return PARSING_DATA;
+	}
+	else if (c != ':')
+	{
+		if (!(*strKey))
+		{
+			*strKey = string_new(KEY_CAPACITY);
+		}
+
+		// Wenn kein Doppelpunkt, dann Zeichen an den String anhängen
+		string_add_char(*strKey, tolower(c));
+
+		// Weiterhin den Key des Feldes parsen
+		return PARSING_FIELD_KEY;
+	}
+	else
+	{
+		// Wenn ein Doppelpunkt gefunden wurde, den Value parsen
+		return PARSING_FIELD_VALUE;
+	}
+}
+
+HttpRequestParsingState parsing_field_value(char c, string** strKey, string** strValue, HttpRequest** httpRequest)
+{
+	if (c != '\n')
+	{
+		if (!(*strValue))
+		{
+			*strValue = string_new(VALUE_CAPACITY);
+		}
+
+		// Wenn keine neue Zeile, dann Zeichen an den String anhängen
+		string_add_char(*strValue, c);
+
+		// Weiterhin den Value des Feldes parsen
+		return PARSING_FIELD_VALUE;
+	}
+	else
+	{
+		string_strip(*strValue);
+
+		// strKey und strValue werden am Ende von der Hashmap freigegeben
+		Hash pair = SH_create(*strKey, *strValue);
+		if ((*httpRequest)->fields == NULL)
+		{
+			(*httpRequest)->fields = SHL_create(pair);
+		}
+		else
+		{
+			SHL_append((*httpRequest)->fields, pair);
+		}
+
+		*strKey = NULL;
+		*strValue = NULL;
+
+		// Wenn eine neue Zeile anfängt, wieder den Key parsen
+		return PARSING_FIELD_KEY;
+	}
+}
 
 //
 // Request-Aufbau
@@ -125,123 +266,23 @@ HttpResponseCodes parse_http_request(char* buffer, size_t bufferSize, HttpReques
         switch (parsingState)
         {
             case PARSING_METHOD:
-                if (c != ' ')
-                {
-                    // Wenn kein Leerzeichen, dann Zeichen an den String anhängen
-                    string_add_char(strMethod, c);
-                }
-                else
-                {
-                    httpRequest->method = get_method_from_string(strMethod);
-                    if (httpRequest->method != GET)
-                    {
-                    	responseCode = NOT_IMPLEMENTED;
-                    }
-
-                    // Wenn ein Leerzeichen gefunden wurde, den Pfad parsen
-                    parsingState = PARSING_PATH;
-                }
+                parsingState = parsing_method(c, strMethod, &httpRequest, &responseCode);
                 break;
 
             case PARSING_PATH:
-                if (c != ' ')
-                {
-                	if (!strPath)
-	                {
-		                strPath = string_new(PATH_CAPACITY);
-	                }
-
-                    // Wenn kein Leerzeichen, dann Zeichen an den String anhängen
-                    string_add_char(strPath, c);
-
-                	if (strPath->len > PATH_CAPACITY)
-	                {
-                		responseCode = BAD_REQUEST;
-	                }
-                }
-                else
-                {
-                    url_decode(strPath);
-
-                    // Wenn ein Leerzeichen gefunden wurde, die Version parsen
-                    parsingState = PARSING_VERSION;
-                }
+            	parsingState = parsing_path(c, &strPath, &httpRequest, &responseCode);
                 break;
 
             case PARSING_VERSION:
-                if (c != '\n')
-                {
-                    // Wenn keine neue Zeile, dann Zeichen an den String anhängen
-                    string_add_char(strVersion, c);
-                }
-                else
-                {
-                    httpRequest->version = validate_version(strVersion);
-                    if (httpRequest->version == UNSUPPORTED)
-                    {
-                    	responseCode = BAD_REQUEST;
-                    }
-
-                    // Wenn eine neue Zeile beginnt, Key eines Feldes parsen
-                    parsingState = PARSING_FIELD_KEY;
-                }
+            	parsingState = parsing_version(c, strVersion, &httpRequest, &responseCode);
                 break;
 
             case PARSING_FIELD_KEY:
-                if (c == '\n')
-                {
-                    // Wenn beim Parsen des Keys eine neue Zeile gefunden wird, sind die Felder zu Ende und es folgen die Daten
-                    parsingState = PARSING_DATA;
-                }
-                else if (c != ':')
-                {
-                	if (!strKey)
-	                {
-                		strKey = string_new(KEY_CAPACITY);
-	                }
-
-                    // Wenn kein Doppelpunkt, dann Zeichen an den String anhängen
-                    string_add_char(strKey, tolower(c));
-                }
-                else
-                {
-                    // Wenn ein Doppelpunkt gefunden wurde, den Value parsen
-                    parsingState = PARSING_FIELD_VALUE;
-                }
+            	parsingState = parsing_field_key(c, &strKey);
                 break;
 
             case PARSING_FIELD_VALUE:
-                if (c != '\n')
-                {
-                	if (!strValue)
-	                {
-                		strValue = string_new(VALUE_CAPACITY);
-	                }
-
-                	// Wenn keine neue Zeile, dann Zeichen an den String anhängen
-                    string_add_char(strValue, c);
-                }
-                else
-                {
-                    strValue = string_strip(strValue);
-
-                    // strKey und strValue werden am Ende von der Hashmap freigegeben
-                    Hash pair = SH_create(strKey, strValue);
-                    if (httpRequest->fields == NULL)
-                    {
-                        httpRequest->fields = SHL_create(pair);
-                    }
-                    else
-                    {
-                        SHL_append(httpRequest->fields, pair);
-                    }
-
-                    strKey = NULL;
-                    strValue = NULL;
-
-                    // Wenn eine neue Zeile anfängt, wieder den Key parsen
-                    parsingState = PARSING_FIELD_KEY;
-                }
+            	parsingState = parsing_field_value(c, &strKey, &strValue, &httpRequest);
                 break;
 
             case PARSING_DATA:
@@ -278,8 +319,6 @@ HttpResponseCodes parse_http_request(char* buffer, size_t bufferSize, HttpReques
 //		    }
 //	    }
 //    }
-
-
 
 	if (responseCode == OK)
 	{
