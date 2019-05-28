74eb8adcd7bfae6314be2ade70297f96430f68fb
Luise <lk323339@fh-muenster.de>
2019-05-28 13:58:05 +0200

    authorization.h /.c mittagspause ;)

--- authorization.c
+++ authorization.c
@@ -3,3 +3,35 @@
 //
 
 #include "authorization.h"
+int abfrage_authorizaition (HashList * hashlist ){
+    Hash * hash= SHL_find_key_cstr(hashlist,"authorization");
+    if (hash){
+        return 1;
+    }
+    else
+        return 0;
+}
+
+int passwort_abfrage_authorizaition(HashList* hashlist) {
+
+    // DOTO Passwort überprüfen
+
+    if (/*DOTO  Passwort ok*/ 1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+
+}
+
+int authorizaition (HashList * hashlist){
+    int temp = abfrage_authorizaition(hashlist);
+    if (temp == 1){
+        temp  = passwort_abfrage_authorizaition (hashlist);
+        if (temp == 1 ) {
+            return 1;
+        }
+    }
+    return 0;
+}
--- authorization.h
+++ authorization.h
@@ -4,11 +4,14 @@
 
 #ifndef PSE20_AUTHORIZATION_H
 
+#include "hash.h"
 
 
-int abfrage_authorizaition ();
+int abfrage_authorizaition (HashList * hashlist );
 
-int passwort_abfrage_authorizaition();
+int passwort_abfrage_authorizaition(HashList* hashlist) ;
+
+int authorizaition (HashList * hashlist);
 
 #define PSE20_AUTHORIZATION_H
 
